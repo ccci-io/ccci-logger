d0f555c0385efe7655cb8059cbc06296f038321f
ccci-io <i@ccci.io>
2020-04-24 09:49:47 -0600

    Dev commit

---
+++ bank.py
@@ -0,0 +1,42 @@
+import time
+import json
+
+class DataBank:
+    def __init__(self, folder):
+        self.folder = folder
+        self.alerts = self.read('settings')
+        #self.update_alerts()
+        self.flags = {
+            'furnace': {
+                'check': False,
+                'on': False,
+            },
+        }
+        self.sensors = {
+            'temperature': self.alerts['furnace']['off'],
+            'humidity': 0
+        }
+
+    def read(self, name):
+        with open(f'{self.folder}json/{name}.json') as f:
+            return json.load(f)
+
+    def write(name, data):
+        with open(f'{self.folder}json/{name}.json', 'w') as f:
+            json.dump(data, f, indent=4)
+
+    def append(name, data):
+        with open(f'{self.folder}json/{name}.json', 'a') as f:
+            json.dump(data, f)
+            f.write(',\n')
+
+    def update_alerts(self):
+        self.alerts = self.read('settings')
+
+    def log(self, name):
+        data = {
+            'ts': int(time.time()),
+            'sensors': self.sensors,
+            'flags': self.flags,
+        }
+        self.append('log', data)
--- controller.py
+++ controller.py
@@ -6,6 +6,7 @@ import json
 
 from adafruit_si7021 import SI7021
 from display import I2C_OLED
+from bank import DataBank
 
 def beep(boop):
     if boop:
@@ -13,29 +14,15 @@ def beep(boop):
     else:
         return True
 
-def read(name):
-    with open(f'{folder}json/{name}.json') as f:
-        return json.load(f)
+def check(task):
+    alert = data.alerts[task]
+    flag = data.flags[task]
+    value = data.sensors[alert['furnace']['sensor']]
 
-def write(name, settings):
-    with open(f'{folder}json/{name}.json', 'w') as f:
-        json.dump(settings, f, indent=4)
-
-def log_data(data):
-    with open(folder + 'json/log.json', 'a') as f:
-        log = {
-            'ts': int(time.time()),
-            'sensors': data['sensors'],
-            'flags': data['flags'],
-        }
-        json.dump(log, f)
-        f.write(',\n')
-
-
-
-def check(data, value, task, boo):
-    alert = data['alert'][task]
-    flag = data['flag'][task]
+    if flag['on']:
+        boo = False
+    else:
+        boo = True
 
     if boo:                             # If furnace is [off]
         arg = value < alert['on']       # TRUE if colder than [on] alert
@@ -55,46 +42,26 @@ def check(data, value, task, boo):
 
 
 def monitor():
-    boop = data['flags']['boop']
-    boop = beep(boop)
+    #boop = data.flags['boop']
+    #boop = beep(boop)
     oled.temperature(sensor.temperature)
 
     print(f"\nTemperature: {round(sensor.temperature, 2)} C")
     print(f"Humidity: {round(sensor.relative_humidity, 2)} %")
+    print(data.flags)
 
-    data['sensors'] = {
-        'temp': round(sensor.temperature, 2),
+    data.sensors = {
+        'temperature': round(sensor.temperature, 2),
         'humidity': round(sensor.relative_humidity, 2),
     }
 
 def operate():
-    if data['flags']['furnace']['on']:
-        check(data, data['sensors']['temp'], 'furnace', False)
-    else:
-        check(data, data['sensors']['temp'], 'furnace', True)
-    log_data(data)
+    check('furnace')
+    log(data)
 
 folder = (__file__)[0:-13]
-
-boop = False
 
 i2c = busio.I2C(board.SCL, board.SDA)   # Create library object using Adafruit Bus I2C port
 sensor = SI7021(i2c)
 oled = I2C_OLED(128, 64, i2c, folder)
-
-data = {
-    'alerts': read('settings'),
-    'flags': {
-        'furnace': {
-            'check': False,
-            'on': False,
-        },
-        'boop': False,
-    },
-    'sensors': {
-        'temp': 0,
-        'humidity': 0,
-    },
-}
-
-data['sensors']['temp'] = data['alerts']['furnace']['off']
+data = DataBank(folder)
