c5019bb1823bcd9083a5cfded396a39b83d929d4
“ccci-io” <i@ccci.io>
2020-08-06 09:18:47 -0600

    Commit comments

--- check.py
+++ check.py
@@ -2,8 +2,6 @@ from datetime import datetime, timedelta
 
 class TaskBot:
 
-    forward = 7 # DAYS TO LOOK AHEAD
-
     def __init__(self, TASKS):
         self.TASKS = TASKS
 
@@ -14,22 +12,17 @@ class TaskBot:
         return self.ls.pop(0)
 
     def schedule(self, days_forward):
-        self.dt = datetime.now()
-        self.now = {
-            'second': dt.second(),
-            'minute': dt.minute(),
-            'hour': dt.hour(),
-            'day': dt.day(),
-            'month': dt.month(),
-            'year': dt.year(),
-            'weekday': dt.isoweekday(),
-        }
+        self.now = datetime.now()
+        
+        self.start = self.now.replace(minute=0, hour=0)
+        noon =  self.start + timedelta(days=1)   #12AM
+        self.reset = noon
+        
         self.ls = []
         tasks = self.TASKS.copy()
 
-        # Check if less then 1 week
-
         #datetime.timedelta(days=0, seconds=0, microseconds=0, milliseconds=0, minutes=0, hours=0, weeks=0)
+        #self.now.replace(minute=0, hour=0)
         for task in tasks:
             if type(task['action']) == str:
                 self.create_task(task)
@@ -41,34 +34,163 @@ class TaskBot:
         
         action = task.pop('action')
         
-        o = 0
-        for i, k in enumerate(list(self.now.keys())):
+        bin_keys = [
+            'minute',   # 1
+            'hour',     # 2
+            'day',      # 4
+            'month',    # 8
+            'year',     # 16
+            'weekday',  # 32
+        ]
+
+        replace = {
+            'millisecond': 0,
+            'second': 0,
+        }
+
+        
+       
+        for i, k in enumerate(bin_keys):
             if k in task:
                 o += 2**i
 
-        if o < 2:
-            for i in range(self.forward*24):
-                delta = self.dt.replace(**task) + timedelta(hours=i) - self.dt
-                if delta > 0 and delta < timedelta(days=self.forward):
-                    self.ls.append([delta, action])
+        on = ''
+
+        for key in bin_keys[::-1]:
+            n = 2**bin_keys.index(key)
+            if o >= n:
+                if key == 'weekday':
+                    weekday = task.pop('weekday')
+                
+                if on != '':
+                    on = key
+                o -= n
+                #getattr(dt, key)()
+                #dt = datetime.now()
+                # REPLACE POP
+                #dt = dt.replace(**replace) + task[key]
+
+        dt = datetime.now()
+
+        if on == 'minute':
+            
+            for i in range(bin_keys.index(on), 4):
+                replace[bin_keys[i]] = 0
+            dt = dt.replace(**replace) + timedelta(task)
         
-        if o < 4:
-            for i in range(self.forward):
-                delta = self.dt.replace(**task) + timedelta(days=i) - self.dt
-                if delta > 0 and delta < timedelta(days=self.forward):
-                    self.ls.append([delta, action])
-                    
+        if on == 'hour':
+
+            for i in range(bin_keys.index(on), 4):
+                replace[bin_keys[i]] = 0
+            dt = dt.replace(**replace) + timedelta(task)
+
+        date_keys = ['year', 'month', 'day']
+        time = {}
+
+        if on == 'day':
+            # IS TODAY THIS DAY?
+            # IS TODAY THIS WEEK?
+
+            if dt < self.now:
+                dt += timedelta(hours=1)
+
+            while dt < self.reset:
+                dt += timedelta(hours=1)
+                task['action']
+
+        if on == 'year':
+            # IS IT ON THIS YEAR?
+            # IF SO IS IT
+            match = True
+            #################################################
+            #### LOOP THROUGH ALL BUT MOVE TIME ELEMENTS TO TIME
+            for key in list(task.keys()):
+                if key in date_keys:
+                    if task[key] != getattr(dt, key)() or task[key] == 0:
+                        match = False
+                else:
+                    if task[key] != 0:
+                        time[key] = task.pop(key)
+                        
+            if match:
+                task_dt = self.start + timedelta(**time)
+                self.ls.append([task_dt, task])
+  
+            # task['year'] == getattr(dt, 'year')()
+            # # # # # # # # # # # # # #
+            ###########################
+
+
+
+
+
+        if dt < self.now:
+            dt += timedelta(hours=1)
+
+        while dt < self.reset:
+            dt += timedelta(hours=1)
+            task['action']
+
 
         if o >= 32:
+            
+            if every != '':
+                on = 'weekday'
+            o -= 32
+
+        if o >= 16:       # Do a task on this YEAR
+            #do things
+        
+        elif o >= 8:        # Do a task every year on this MONTH.
+            #do things
+        
+        elif o >= 4:        # Do a task every month on this DAY.
+
+        elif o >= 2:        # Do a task every day on this HOUR.
+
+            for i in range(self.forward*24):
+                delta = self.now.replace(**task) + timedelta(hours=i) - self.now
+                if delta > 0 and delta < timedelta(days=self.forward):
+                    self.ls.append([delta, action])
+
+        elif o = 1:         # Do a task every hour on this MINUTE.
+            dt = datetime.now()
+            dt = dt.replace(**replace) + task['minute']
+            if dt < self.now:
+                dt += timedelta(hours=1)
+
+            while dt < self.reset:
+                dt += timedelta(hours=1)
+                task['action']
+        
+
+
+        replace['day'] = 0
+
+        elif 4 <= o < 8:    # Do a task every month on this DAY.
+            for i in range(self.forward):
+                delta = self.now.replace(**task) + timedelta(days=i) - self.now
+                if delta > 0 and delta < timedelta(days=self.forward):
+                    self.ls.append([delta, action])
+
+        replace['month'] = 0
+        
+        elif 8 <= o < 16:   # Do a task every year on this MONTH.
+            # Do things
+            
+        elif 16 <= o < 32:  # Do a task on this YEAR
+            # Do things
+
+        if 32 <= o:
             weekday = task.pop('weekday')
 
-        if o >= 36:
+        elif 36 <= o:
             # Additional operation to compare if day/month/year match the weekday. (in Jan 2020 Sundays 5pm send data)
             'hello'
                 
 
     def check_state(self, task):
-        arg = task=['hour'][0] < self.now['hour'] < task['hour'][1]
+        arg = task['hour'][0] < self.now.hour() < task['hour'][1]
 
 
 
